481863622e39d0526d311312e28c2b320e4ffe0f
AvielSheto <avielsheto@gmail.com>
2023-04-19 23:58:44 +0300

    Update

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -4,7 +4,7 @@ import { useRouter } from 'expo-router';
 
 import styles from './popularjobs.style';
 import { COLORS, SIZES } from '../../../constants';
-import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard'
 
 const Popularjobs = () => {
   const router = useRouter();
