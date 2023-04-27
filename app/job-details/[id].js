9523bb3a821ecf0f4bf2a0d041114b4e6919c956
AvielSheto <avielsheto@gmail.com>
2023-04-27 22:50:03 +0300

    Update job page root

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,8 +1,50 @@
-import React from 'react'
+import { View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl } from 'react-native';
+import { Stack, useRouter, useSearchParams } from 'expo-router';
+import { useCallback, useState } from 'react';
+
+import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from '../../components';
+import { COLORS, icons, SIZES } from '../../constants';
+import useFetch from '../../hook/useFetch';
 
 function jobDetails() {
+  const params = useSearchParams();
+  const router = useRouter();
+
+  const { data, isLoading, error, refetch } = useFetch('job-details', { job_id: params.id });
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+
+  }
+
   return (
-    <div>jobDetails</div>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension="60%"
+              handlePress={() => router.back()}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.share}
+              dimension="60%"
+            />
+          ),
+          headerTitle: ''
+        }}
+      />
+     <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}>
+      {isLoading ?ActivityIndicator}
+
+     </ScrollView>
+    </SafeAreaView>
   )
 }
 
