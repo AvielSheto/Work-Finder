48343420236e4cc4dec00e95f8e21d005681b5b5
AvielSheto <avielsheto@gmail.com>
2023-04-29 14:56:12 +0300

    Creating Company component

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -40,10 +40,31 @@ function jobDetails() {
           headerTitle: ''
         }}
       />
-     <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}>
-      {isLoading ?ActivityIndicator}
+      <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : data.length === 0 ? (
+          <Text>No data</Text>
+        ): (
+          <View style={{padding: SIZES.medium, paddingBottom: 100}}>
+            <Company
+            companyLogo={data[0].employer_logo}
+            jobTitle={data[0].job_title}
+            companyName={data[0].employer_name}
+            location={data[0].job_country}
+            />
 
-     </ScrollView>
+            <JobTabs
+            
+            />
+
+          </View>
+        )
+    }
+
+      </ScrollView>
     </SafeAreaView>
   )
 }
