520ddf68a8ecf49a1842b97fe62697dd480daf7f
AvielSheto <avielsheto@gmail.com>
2023-04-30 12:17:56 +0300

    Update Tabs in jobDetails

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -5,6 +5,7 @@ import { useCallback, useState } from 'react';
 import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from '../../components';
 import { COLORS, icons, SIZES } from '../../constants';
 import useFetch from '../../hook/useFetch';
+const tabs = ["About", "Qualifications", "Responsibilities"];
 
 function jobDetails() {
   const params = useSearchParams();
@@ -13,6 +14,7 @@ function jobDetails() {
   const { data, isLoading, error, refetch } = useFetch('job-details', { job_id: params.id });
 
   const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
 
   const onRefresh = () => {
 
@@ -22,51 +24,54 @@ function jobDetails() {
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
         options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
           headerShadowVisible: false,
           headerBackVisible: false,
           headerLeft: () => (
             <ScreenHeaderBtn
               iconUrl={icons.left}
-              dimension="60%"
+              dimension='60%'
               handlePress={() => router.back()}
             />
           ),
           headerRight: () => (
-            <ScreenHeaderBtn
-              iconUrl={icons.share}
-              dimension="60%"
-            />
+            <ScreenHeaderBtn iconUrl={icons.share} dimension='60%' />
           ),
-          headerTitle: ''
+          headerTitle: "",
         }}
       />
-      <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
-        {isLoading ? (
-          <ActivityIndicator size="large" color={COLORS.primary} />
-        ) : error ? (
-          <Text>Something went wrong</Text>
-        ) : data.length === 0 ? (
-          <Text>No data</Text>
-        ) : (
-          <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
-            <Company
-              companyLogo={data[0].employer_logo}
-              jobTitle={data[0].job_title}
-              companyName={data[0].employer_name}
-              location={data[0].job_country}
-            />
 
-            <JobTabs
+      <>
+        <ScrollView showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+        >
+          {isLoading ? (
+            <ActivityIndicator size='large' color={COLORS.primary} />
+          ) : error ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data available</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
 
-            />
-
-          </View>
-        )
-        }
-
-      </ScrollView>
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+            </View>
+          )}
+        </ScrollView>
+      </>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 export default jobDetails
