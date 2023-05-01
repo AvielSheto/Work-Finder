46895791bfc7a8b35967095b2b018417c3fd786d
AvielSheto <avielsheto@gmail.com>
2023-05-01 11:02:31 +0300

    Update About Component in the job

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -20,6 +20,25 @@ function jobDetails() {
 
   }
 
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return (
+          <Specifics
+            title='Qualifications'
+            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+          />
+        )
+
+        case "About":
+          return(
+            <JobAbout
+            info={data[0].job_description ?? "No data provided"}
+            />
+          )
+    }
+  }
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -66,6 +85,8 @@ function jobDetails() {
                 activeTab={activeTab}
                 setActiveTab={setActiveTab}
               />
+
+              {displayTabContent()}
             </View>
           )}
         </ScrollView>
